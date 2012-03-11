5d053ce08f225569fbcfc1a2966574ea3a498f90
Guannan Ren <gren@redhat.com>
2012-03-11 20:26:46 +0800

    quote variable, remove use of local command

--- cron/nightly/libvirt_cron_testing.sh
+++ cron/nightly/libvirt_cron_testing.sh
@@ -22,15 +22,15 @@ source_dist()
     srcdir=$1
     shift
 
-    autogen_f=$srcdir/$autogen
-    echo "execute $autogen_f" | tee -a $logfile
+    autogen_f="$srcdir/$autogen"
+    echo "execute $autogen_f" | tee -a "$logfile"
 
     if [ ! -e "$autogen_f" ] || [ ! -x "$autogen_f" ]; then
-        echo "autogen.sh in $srdir doesn't exist or couldn't be executed" | tee -a $logfile
+        echo "autogen.sh in $srdir doesn't exist or couldn't be executed" | tee -a "$logfile"
         return 1
     else
         (cd "$distdir"
-        $autogen_f
+        "$autogen_f"
         if [ $? -ne 0 ]; then
             exit 1
         fi
@@ -38,7 +38,7 @@ source_dist()
         if [ $? -ne 0 ]; then
             exit 1
         fi
-        ) | tee -a $logfile
+        ) | tee -a "$logfile"
         if [ $? -ne 0 ]; then
             return 1
         fi
@@ -53,13 +53,13 @@ git_pull()
     shift
 
     if [ ! -d "$srcdir/.git" ]; then
-        echo "$srcdir is not a git direcotry" | tee -a $logfile
+        echo "$srcdir is not a git direcotry" | tee -a "$logfile"
         return 1
     else
-        echo "cd $srcdir" | tee -a $logfile
-        echo "running \"git pull\"..." | tee -a $logfile
+        echo "cd $srcdir" | tee -a "$logfile"
+        echo "running \"git pull\"..." | tee -a "$logfile"
         (cd "$srcdir"
-         git pull) | tee -a $logfile
+         git pull) | tee -a "$logfile"
 
         if [  $? -ne 0 ]; then
             return 1
@@ -71,25 +71,25 @@ git_pull()
 
 dist_check()
 {
-    if ! ls $distdir/libvirt-*.tar.gz > /dev/null; then
+    if ! ls "$distdir"/libvirt-*.tar.gz > /dev/null; then
         return 1
     fi
 }
 
 distribute()
 {
-    rm -rf $targetdir/libvirt-*.tar.gz
-    if ! cp -f $distdir/libvirt-*.tar.gz "$targetdir" > /dev/null; then
+    rm -rf "$targetdir"/libvirt-*.tar.gz
+    if ! cp -f "$distdir"/libvirt-*.tar.gz "$targetdir" > /dev/null; then
         return 1
     fi
 }
 
 machine_check()
 {
-    local m="$1"
+    m="$1"
     shift
 
-    if ! ping -c 3 $m > /dev/null; then
+    if ! ping -c 3 "$m" > /dev/null; then
         return 1
     fi
 }
@@ -97,7 +97,7 @@ machine_check()
 autotest_prepare()
 {
     rm_auto_cmd="rm -rf $autotestdir"
-    "$SSH" -l root "$MACHINE" "$rm_auto_cmd" | tee -a $logfile
+    "$SSH" -l root "$MACHINE" "$rm_auto_cmd" | tee -a "$logfile"
     if [ $? -ne 0 ]; then
         return 1
     fi
@@ -108,42 +108,42 @@ submit_job()
     retval=0
     cli="$autotestcli/atest"
     START_DATE=$(date +%m/%d/%Y" "%T)
-    echo "Job start at $START_DATE" | tee -a $logfile
+    echo "Job start at $START_DATE" | tee -a "$logfile"
 
     for testsuit in $(echo "$TESTSUITS" | tr "," "\n")
     do
         echo "Running: $PYTHON $cli job create -t $testsuit \
--m $MACHINE \"$TESTNAME($testsuit)\" --web=$AUTOTEST_SERVER" | tee -a $logfile
+-m $MACHINE \"$TESTNAME($testsuit)\" --web=$AUTOTEST_SERVER" | tee -a "$logfile"
         ("$PYTHON" "$cli" job create \
                              -t "$testsuit" \
                              -m "$MACHINE" \
                              "$TESTNAME($testsuit)" \
                              --web="$AUTOTEST_SERVER"
-        ) | tee -a $logfile
+        ) | tee -a "$logfile"
         if [ $? -ne 0 ]; then
-            echo "$testsuit submission Failed" | tee -a $logfile
+            echo "$testsuit submission Failed" | tee -a "$logfile"
             retval=1
             break
         fi
-        echo "Success to submit job, job name is $TESTNAME($testsuit)" | tee -a $logfile
+        echo "Success to submit job, job name is $TESTNAME($testsuit)" | tee -a "$logfile"
 
-        echo "Checking job status:" | tee -a $logfile
+        echo "Checking job status:" | tee -a "$logfile"
         sleep_time=1
         while true
         do
             running_job=$("$PYTHON" "$cli" job list -r -u autotest --web="$AUTOTEST_SERVER" | sed -n '$p')
             if [ -z "$running_job" ]; then
                 result_state=$("$PYTHON" "$cli" job list -u autotest --web="$AUTOTEST_SERVER" | sed -n '$p')
-                echo "$testsuit :$result_state" | tee -a $logfile
+                echo "$testsuit :$result_state" | tee -a "$logfile"
 
                 if echo "$result_state" | grep "Completed" > /dev/null; then
                     # sleep 10 secs for testing machine boot
-                    echo "Waiting 1 min for testing machine reboot"
+                    echo "Waiting 1 min for testing machine reboot" | tee =a "$logfile"
                     sleep 60
                     if "$SSH" -l root "$MACHINE" ls "$install_flag" > /dev/null; then
                         break
                     else
-                        echo "Libvirt install testing FAIL!" | tee -a $logfile
+                        echo "Libvirt install testing FAIL!" | tee -a "$logfile"
                         retval=1
                         break 2
                     fi
@@ -153,39 +153,39 @@ submit_job()
                 fi
             else
                 if [ "$sleep_time" -eq 1 ] && echo "$running_job" | grep "Running" > /dev/null; then
-                    echo "$testsuit :$running_job" | tee -a $logfile
+                    echo "$testsuit :$running_job" | tee -a "$logfile"
                     sleep_time=10
                 fi
             fi
-            sleep $sleep_time
+            sleep "$sleep_time"
         done
     done
 
     END_DATE=$(date +%m/%d/%Y" "%T)
-    echo "Job end at $END_DATE" | tee -a $logfile
-    echo "$retval"
-    return $retval
+    echo "Job end at $END_DATE" | tee -a "$logfile"
+    echo "$retval" | tee -a "$logfile"
+    return "$retval"
 }
 
 init()
 {
-   : >$logfile
-   echo "Initiate testing environment" | tee -a $logfile
+   : >"$logfile"
+   echo "Initiate testing environment" | tee -a "$logfile"
 
-   if [ -d $distdir ]; then
-       if [ -f  $distdir/Makefile ]; then
-           echo "execute \"make maintainer-clean...\"" | tee -a $logfile
+   if [ -d "$distdir" ]; then
+       if [ -f  "$distdir/Makefile" ]; then
+           echo "execute \"make maintainer-clean...\"" | tee -a "$logfile"
            current_d=$(pwd)
            cd "$distdir"
            make maintainer-clean > /dev/null;
            cd "$current_d"
        fi
 
-       echo "remove old $distdir" | tee -a $logfile
-       rm -rf $distdir
+       echo "remove old $distdir" | tee -a "$logfile"
+       rm -rf "$distdir"
    fi
-   echo "make new $distdir" | tee -a $logfile
-   mkdir $distdir
+   echo "make new $distdir" | tee -a "$logfile"
+   mkdir "$distdir"
 }
 
 usage()
@@ -205,7 +205,7 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-while getopts :hd:m:t: option
+while getopts hd:m:t: option
 do
     case "$option" in
     h)
@@ -221,8 +221,7 @@ do
     t)
         TESTSUITS="$OPTARG"
         ;;
-    *)
-        echo "Invaid option"
+    \?)
         usage
         exit 1
         ;;
@@ -241,61 +240,61 @@ if [ -z "$TESTSUITS" ]; then
 fi
 echo "Testsuit is $TESTSUITS"
 
+init
+
 if ! machine_check "$MACHINE"; then
-    echo "Failed to ping machine $MACHINE" | tee -a $logfile
+    echo "Failed to ping machine $MACHINE" | tee -a "$logfile"
     exit 1
 fi
 
-init
-
-srcdir=${srcdir-$defaultsrcdir}
-echo "Libvirt source code direcotry is $srcdir" | tee -a $logfile
-echo "" >> $logfile
+: ${srcdir:="$defaultsrcdir"}
+echo "Libvirt source code direcotry is $srcdir" | tee -a "$logfile"
+echo "" >> "$logfile"
 
 git_pull "$srcdir"
 if [ $? -ne 0 ]; then
-    echo "Failed to pull git update" | tee -a $logfile
+    echo "Failed to pull git update" | tee -a "$logfile"
     exit 1
 fi
-echo "Success to git pull" | tee -a $logfile
-echo "" >> $logfile
+echo "Success to git pull" | tee -a "$logfile"
+echo "" >> "$logfile"
 
-echo "Make distribution..." | tee -a $logfile
+echo "Make distribution..." | tee -a "$logfile"
 source_dist "$srcdir"
 if [ $? -ne 0 ]; then
-    echo "Failed to make distribution" | tee -a $logfile
+    echo "Failed to make distribution" | tee -a "$logfile"
     exit 1
 fi
-echo "Success to make distribution" | tee -a $logfile
-echo "" >> $logfile
+echo "Success to make distribution" | tee -a "$logfile"
+echo "" >> "$logfile"
 
 if ! dist_check; then
-    echo "No libvirt tarball is produced in $distdir" | tee -a $logfile
+    echo "No libvirt tarball is produced in $distdir" | tee -a "$logfile"
     exit 1
 fi
 
-echo "Copy libvirt tarball to $targetdir" | tee -a $logfile
+echo "Copy libvirt tarball to $targetdir" | tee -a "$logfile"
 if ! distribute; then
-    echo "Failed to copy libvirt tarball to $targetdir" | tee -a $logfile
+    echo "Failed to copy libvirt tarball to $targetdir" | tee -a "$logfile"
     exit 1
 fi
-echo "Done with copying libvirt tarball" | tee -a $logfile
-echo "" >> $logfile
+echo "Done with copying libvirt tarball" | tee -a "$logfile"
+echo "" >> "$logfile"
 
-echo "Remove the old autotest on testing machine" | tee -a $logfile
+echo "Remove the old autotest on testing machine" | tee -a "$logfile"
 if ! autotest_prepare; then
-    echo "Failed to remove $autotestdir on machine $MACHINE" | tee -a $logfile
+    echo "Failed to remove $autotestdir on machine $MACHINE" | tee -a "$logfile"
     exit 1
 fi
 
-echo "Success to remove $autotestdir on machine $MACHINE" | tee -a $logfile
-echo "" >> $logfile
+echo "Success to remove $autotestdir on machine $MACHINE" | tee -a "$logfile"
+echo "" >> "$logfile"
 
-echo "Testsuit list: $TESTSUITS" | tee -a $logfile
+echo "Testsuit list: $TESTSUITS" | tee -a "$logfile"
 submit_job
 if [ $? -ne 0 ]; then
     exit 1
 fi
-echo ""
+echo "" >> "$logfile"
 
 exit 0
